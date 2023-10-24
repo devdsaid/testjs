e317d322824604552a61068a9ed5ab13176f2595
devdsaid <sazairasa15@gmail.com>
2023-10-24 08:55:27 +0300

    new file index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const onj = users[{
+    id: 1,
+    name: 'Рамазан',
+    age: 25
+}, {
+    id: 2,
+    name: 'Ибрагим',
+    age: 26
+}, {
+    id: 3,
+    name: 'Артур',
+    age: 28
+}, {
+    id: 4,
+    name: 'Батыр',
+    age: 29
+}, ]
